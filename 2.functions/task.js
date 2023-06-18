eed9cc23c8ad878872dfa9c30a61664dc59a2334
Digitamax-group <74538438+Digitamax-group@users.noreply.github.com>
2023-06-18 20:32:44 +0300

    Update task.js
    Второе задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,54 @@
-function getArrayParams(...arr) {
-
+// Задание 1
+function getArrayParams(arr) {
+  let min, max, sum, avg;
+  min = Infinity;
+  max = - Infinity;
+  sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    if (arr[i] > max) {
+      max = arr[i];
+    };
+    if (arr[i] < min) {
+      min = arr[i];
+    };
+  };
+  avg = (sum / arr.length).toFixed(2);
+  avg = Number(avg);
   return { min: min, max: max, avg: avg };
 }
 
-function summElementsWorker(...arr) {
-
+// Задание 2
+function worker(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 }
 
-function differenceMaxMinWorker(...arr) {
-
+function makeWork(arrOfArr, func) {
+  let max = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {  
+    let f = func(arrOfArr[i]);
+    if ( f > max) {
+      max = f;
+    }
+  }  
+  return max;
 }
 
-function differenceEvenOddWorker(...arr) {
-
-}
-
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
+// Задание 3
+function worker2(arr) {
+  let min = Infinity;
+  let max = - Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return Math.abs(max - min);
 }
