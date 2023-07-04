fa065fdc21b202d875a79588e382e39b769fba9b
Digitamax <kiselevmaxim4@yandex.ru>
2023-07-04 17:49:18 +0300

    Update task.js
    2nd update task 8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,52 +1,49 @@
+//Задача № 1
 function cachingDecoratorNew(func) {
 	let cache = [];
-
+  
+	return function (...args) {
+	  const hash = md5(args);
+	  let objectInCache = cache.find(
+		(objectInCache) => objectInCache.hash === hash
+	  );
+  
+	  if (objectInCache) {
+		console.log("Из кэша: " + objectInCache.result);
+		return "Из кэша: " + objectInCache.result;
+	  }
+  
+	  let result = func(...args);
+	  cache.push({ hash, result });
+  
+	  if (cache.length > 5) {
+		cache.shift();
+	  }
+	  console.log("Вычисляем: " + result);
+	  return "Вычисляем: " + result;
+	};
+  }
+  //Задача № 2
+  function debounceDecoratorNew(func, delay) {
+	let timeoutId = null;
 	function wrapper(...args) {
-		const hash = args.join(',');
-		let idx = cache.findIndex((item) => item.hash == hash);
-		if (idx !== -1) {
-			console.log("Из кэша: " + cache[idx].result);
-			return "Из кэша: " + cache[idx].result;
-		}
-
-		let result = func(...args);
-		cache.push({
-			hash,
-			result
-		});
-		if (cache.length > 5) {
-			cache.shift();
-		}
-		console.log("Вычисляем: " + result);
-		return "Вычисляем: " + result;
-	}
-	return wrapper;
-}
-
-function debounceDecoratorNew(func, ms) {
-	let timer = null;
-
-	function wrapper(...args) {
-		if (timer === null) {
-			func(...args);
-		}
-		clearTimeout(timer);
-		timer = setTimeout(() => func(...args), ms);
-	}
-	return wrapper;
-}
-
-function debounceDecorator2(func, ms) {
-	let timer = null;
-
-	function wrapper(...args) {
-		if (timer === null) {
-			func(...args);
-		}
-		clearTimeout(timer);
-		timer = setTimeout(() => func(...args), ms);
+	  if (timeoutId === null) {
+		func(...args);
 		wrapper.count++;
+	  }
+	  if (timeoutId) {
+		clearTimeout(timeoutId);
+	  }
+  
+	  timeoutId = setTimeout(() => {
+		timeoutId = true;
+		func(...args);
+		wrapper.count++;
+	  }, delay);
+	  wrapper.allCount++;
 	}
+  
 	wrapper.count = 0;
+	wrapper.allCount = 0;
 	return wrapper;
-}
+  }
